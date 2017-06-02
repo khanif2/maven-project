0afea51462266b230b39e3dc85f46d9fef8aa452
khawar hanif <khawar.hanif@csagroup.org>
2017-06-02 07:28:25 -0400

    test failure and jinkin Test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -8,23 +8,24 @@ import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
-    @Test
-    public void mavenFlowReturnsHelloMaven() throws Exception {
-       //ystem.out.print("mavenFlowReturnsHelloMaven");
-    	runFlowAndExpect("mavenFlow", "Hello World");
-    }
+	@Test
+	public void mavenFlowReturnsHelloMaven() throws Exception {
+		// ystem.out.print("mavenFlowReturnsHelloMaven");
+		runFlowAndExpect("mavenFlow", "Hello World");
+	}
 
-    @Test
-    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
-    	  	MuleEvent event = runFlow("retrieveFlights");
-    	  	 System.out.print("retrieveFlightsAddsAppropriateHeader()");
-    	  	  
-    	  	String contentType = event.getMessage().getOutboundProperty("Content-Type");
-      assertEquals("application/json", contentType);
-    } 
-    @Override
-    protected String getConfigFile() {
-        return "maven-project.xml";
-    }
+	@Test
+	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+		MuleEvent event = runFlow("retrieveFlights");
+		System.out.print("retrieveFlightsAddsAppropriateHeader()");
+
+		String contentType = event.getMessage().getOutboundProperty("Content-Type ");
+		assertEquals("application/json", contentType);
+	}
+
+	@Override
+	protected String getConfigFile() {
+		return "maven-project.xml";
+	}
 
 }
